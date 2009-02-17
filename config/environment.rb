33adc080b40679ce8ae95bf804a2a25a9db2f899
Paul Engel <paul.engel@holder.nl>
2009-02-17 17:36:12 +0100

    Changed database settings

--- config/environment.rb
+++ config/environment.rb
@@ -18,7 +18,7 @@ Rails::Initializer.run do |config|
 
   # Skip frameworks you're not going to use. To use Rails without a database
   # you must remove the Active Record framework.
-  # config.frameworks -= [ :active_record, :active_resource, :action_mailer ]
+  config.frameworks -= [ :active_resource, :action_mailer ]
 
   # Specify gems that this application depends on. 
   # They can then be installed with "rake gems:install" on new installations.
