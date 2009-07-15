1ec176f2334725c3ccd075a04eb4672e0ce79ee8
Paul Engel <paulengel@ArCHanGeL.local>
2009-07-16 01:24:37 +0200

    Completed comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,9 +4,27 @@ class CommentsController < ApplicationController
     @comment = Comment.new
   end
   
+  def new
+    @comment = Comment.new(params[:comment])
+    @comment.valid? if params.include?(:comment)
+    render :layout => false
+  end
+  
   def create
-    (@comment = Comment.new(params[:comment])).save
-    render :action => "index"
+    respond_to do |format|
+      format.html {
+        render :action => "index"
+      }
+      format.js {
+        render :update do |page|
+          unless (@comment = Comment.new(params[:comment])).save
+            page << "TopUp.display('#{new_comment_path}', {parameters: '#{params[:comment].collect{|k, v| "comment[#{k}]=#{v}"}.join "&"}', effect: 'fade', shaded: 1, overlayClose: 1, resizable: 0})"
+          else
+            page.redirect_to comments_path
+          end
+        end
+      }
+    end
   end
   
 end
