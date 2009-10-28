3ec76678682306347ee326e876340e0345f76df9
Paul Engel <paulengel@ArCHanGeL.local>
2009-10-28 23:34:14 +0100

    Using Sass and Jzip (yay!) properly (for the website and the plugin as well) Rearranged javascript and stylesheet files Removed responds_to_parent plugin

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,6 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
   
-  def include_src_for(file)
-    RAILS_ENV == "production" ?
-      "/releases/#{file unless file == "top_up"}/latest/#{file}-min".squeeze("/") :
-      file
-  end
-  
   def h1(text)
     text_tag(:h1, text)
   end
