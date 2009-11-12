e02f0bc9d8c241ba5966a74fdc3b87db4b784040
Paul Engel <paulengel@ArCHanGeL.local>
2009-11-12 21:56:10 +0100

    Fixed page title :P

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,17 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
   
+  def page_title
+    section = controller.action_name == "index" ?
+                begin
+                  name = controller.controller_name.gsub("Controller", "").capitalize
+                  name == "Application" ? "Home" : name
+                end :
+                controller.action_name.capitalize
+    
+    "The #1 Javascript Pop Up - Get TopUp! - #{section}"
+  end
+  
   def menu_items
     %w(home documentation examples demo comments)
   end
