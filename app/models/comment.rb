c372d8029bf247af3b31baa5808de50e2824663f
Paul Engel <paulengel@g195189.upc-g.chello.nl>
2010-01-08 07:33:12 +0100

    Updated spam check

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -24,7 +24,7 @@ class Comment < ActiveRecord::Base
   end
   
   def spam?
-    self.message.match(/(^Very good site\. Thanks\!\,|^Nice site\. Thank you\.|^hi <a[^>]+>[^<\/]+<\/a>\s*$|http\:\/\/(ui\-patterns\.com|tezzels\.net)\/users\/\d+\#\d|freehostia\.com|\b(porn|sex|nude|xxx|fuck|playboy|\[\\url\])\b)/) ||
+    self.message.downcase.match(/(^Very good site\. Thanks\!\,|^Nice site\. Thank you\.|^hi <a[^>]+>[^<\/]+<\/a>\s*$|http\:\/\/(ui\-patterns\.com|tezzels\.net)\/users\/\d+\#\d|freehostia\.com|\b(porn|sex|nude|xxx|fuck|playboy)\b|\[\\url\])/) ||
     self.message.count_matches(/<a([^>]+)?>[^<\/]+<\/a>/) > 8
   end
   
